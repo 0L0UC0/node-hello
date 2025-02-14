1f65fd5f8c06eff6d7edf0a1a5b9961965b50840
Aleck <00001103235916sp@al.educacao.sp.gov.br>
2025-02-14 12:20:24 -0300

    First Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'If you have open this website, you should kill your self\n'
   res.end(msg);
 });
 
